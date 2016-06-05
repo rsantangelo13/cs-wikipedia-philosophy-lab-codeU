efce59350561fd2f2be066c0590e166029d299f9
rsantangelo13 <rsantangelo13@gmail.com>
2016-06-05 12:12:50 -0400

    finished lab

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -30,22 +30,52 @@ public class WikiPhilosophy {
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
-
+		List<String> urls = new ArrayList<String>();
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+		urls.add(url);
+		do{
+			url = findUrl(url);
+			System.out.println(url);
+			if(url == null){
+				url = "Error";
 			}
-        }
-
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+			if(urls.contains(url)){
+				url = "Error";
+			}
+			urls.add(url);
+		}while(url!= "Error" && !url.equals("https://en.wikipedia.org/wiki/Philosophy"));
+		System.out.println(urls);
+	}
+	private static String findUrl(String url)throws IOException{
+		Elements paragraphs = wf.fetchWikipedia(url);
+		Element para;
+		Iterable<Node> iter;
+		int parenthesis = 0;
+		for(int i = 0; i < paragraphs.size(); i++){
+			para = paragraphs.get(i);
+			iter = new WikiNodeIterable(para);
+			for (Node node: iter) {
+				if (node instanceof TextNode) {
+					//System.out.println(node);
+					String text = node.attr("text");
+					for(int j = 0; j<text.length(); j++){
+						if(text.charAt(j) == '(')
+							parenthesis++;
+						else if(text.charAt(j) == ')')
+							parenthesis--;
+					}
+				}
+				else{
+					String wikiName = node.attr("href");
+					if(wikiName.startsWith("/wiki")){
+						String nextUrl = "https://en.wikipedia.org" + node.attr("href");
+						if(!nextUrl.equals(url) && parenthesis == 0){
+							return nextUrl;
+						}
+					}
+				}
+	        }
+		}
+		return null;
 	}
 }
